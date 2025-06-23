ad41269686e6d9873aa7cde140662eff6d595f71
qwert26765 <3895675652@qq.com>
2025-06-23 11:32:02 +0800

    111

--- Assets/Blood.cs
+++ Assets/Blood.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Blood : MonoBehaviour
 {
+    public GameObject blood;
     GameObject game;
     // Start is called before the first frame update
     void Start()
