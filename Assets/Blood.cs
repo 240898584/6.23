ed3f95fb132ff55b0cd0b4d0a172278a195ac4b3
qwert26765 <3895675652@qq.com>
2025-06-23 11:34:06 +0800

    分支

--- Assets/Blood.cs
+++ Assets/Blood.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Blood : MonoBehaviour
 {
     public GameObject blood;
+    GameObject bloodClone;
     GameObject game;
     // Start is called before the first frame update
     void Start()
